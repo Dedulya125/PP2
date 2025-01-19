64d979ac3c6ea9b6b6d97d286ea648f8e51bcb42
Nurbek Tazhenov <ashotoneshot482@gmail.com>
2025-01-20 00:47:47 +0500

    update

--- lab1/tasks.py
+++ lab1/tasks.py
@@ -8,3 +8,34 @@ if 5 > 2:
 #Example 3
 #print("Hello, World!")
 print("Cheers, Mate!")
+
+#Example 4
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
